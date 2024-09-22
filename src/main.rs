27cbc207b09a6d5c61da620c2dd37a1391a34b63
Dima Pristupa <dima@pristupa.dev>
2024-09-22 14:34:05 +0300

    APP_NO_ANSI_COLORS: introduce

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ async fn main() -> Result<()> {
     let port = std::env::var("APP_PORT").with_context(|| "APP_PORT")?;
     let access_token = std::env::var("APP_ACCESS_TOKEN").with_context(|| "APP_ACCESS_TOKEN")?;
     let images_dir = std::env::var("APP_IMAGES_DIR").with_context(|| "APP_IMAGES_DIR")?;
+    let no_colors = std::env::var("APP_NO_ANSI_COLORS").is_err();
 
     tracing_subscriber::fmt()
         .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+        .with_ansi(no_colors)
         .init();
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;

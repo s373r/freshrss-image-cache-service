5cdd879fc2fc7e8913984464497014c74eff6c0d
Alex Besogonov <b@alex.net>
2024-12-02 10:54:32 -0800

    Distribute files into subdirs, and add support for CAPTCHA detection (#1)
    * Spread files across intermediary directories
    
    * Add handling for CAPTCHA errors
    
    * Add a non-TLS-validating endpoint
    
    * Add an ability to work around CloudFlare

--- src/app_service.rs
+++ src/app_service.rs
@@ -1,27 +1,57 @@
+use std::error::Error;
+use std::fmt;
 use crate::dtos::CachedImage;
 use anyhow::{bail, Context, Result};
 use sha3::{Digest, Sha3_256};
 use std::path::PathBuf;
+use axum::http::HeaderValue;
+use reqwest::Response;
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
 use url::Url;
 
 #[derive(Clone)]
 pub struct CachedImagePaths {
+    data_folder: PathBuf,
     data_path: PathBuf,
     mime_type_path: PathBuf,
+}
+
+#[derive(Clone, Debug)]
+pub struct CloudFlareBypassProxy {
+    pub proxy_url: String,
+    pub proxy_login: String,
+    pub proxy_password: String,
 }
 
 pub struct AppService {
     access_token: String,
     images_dir: PathBuf,
+    disable_https_validation: bool,
+    bypass_info: Option<CloudFlareBypassProxy>,
+}
+
+
+#[derive(Debug)]
+struct CaptchaError {
+    mime_type: String,
+}
+impl Error for CaptchaError {}
+
+impl fmt::Display for CaptchaError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Received a CAPTCHA page, mime type: {}", self.mime_type)
+    }
 }
 
 impl AppService {
-    pub fn new(access_token: String, images_dir: PathBuf) -> Self {
+    pub fn new(access_token: String, images_dir: PathBuf, disable_https_validation: bool,
+        bypass_info: Option<CloudFlareBypassProxy>) -> Self {
         Self {
             access_token,
             images_dir,
+            disable_https_validation,
+            bypass_info,
         }
     }
 
@@ -67,10 +97,14 @@ impl AppService {
             hex::encode(&hash[..])
         };
         let mime_type_file_name = format!("{image_content_file_name}.mime-type");
+        // Use two first letters of the file name for the intermediate directory, to
+        // make sure we don't end up with a huge number of files.
+        let data_folder = self.images_dir.join(&image_content_file_name[0..2]);
 
         Ok(CachedImagePaths {
-            data_path: self.images_dir.join(image_content_file_name),
-            mime_type_path: self.images_dir.join(mime_type_file_name),
+            data_path: data_folder.join(image_content_file_name),
+            mime_type_path: data_folder.join(mime_type_file_name),
+            data_folder,
         })
     }
 
@@ -79,12 +113,24 @@ impl AppService {
         image_url: &Url,
         cached_image_paths: CachedImagePaths,
     ) -> Result<()> {
-        let client = reqwest::Client::new();
-        let image_response = client.get(image_url.clone()).send().await?;
+        // Make sure the directory for the file exists
+        fs::create_dir_all(cached_image_paths.data_folder).await?;
 
-        let Some(mime_type) = image_response.headers().get("content-type").cloned() else {
-            bail!("Failed to get the content-type header for image: {image_url}");
-        };
+        let mut res = self.try_get_image(image_url, false).await;
+        if let Err(err) = res {
+            // If not a CAPTCHA error or we can't bypass CloudFlare, return the error
+            if !err.downcast_ref::<CaptchaError>().is_some() || self.bypass_info.is_none() {
+                return Err(err);
+            }
+
+            // Try to use the CloudFlare bypass
+            res = self.try_get_image(image_url, true).await;
+            if let Err(err) = res {
+                return Err(err);
+            }
+        }
+
+        let (image_response, mime_type) = res?;
 
         // 1. Save the image content
         let mut image_file = fs::File::create(cached_image_paths.data_path.clone())
@@ -108,5 +154,38 @@ impl AppService {
         fs::write(cached_image_paths.mime_type_path, mime_type).await?;
 
         Ok(())
+    }
+
+    async fn try_get_image(&self, image_url: &Url, deploy_workarounds: bool) -> Result<(Response, HeaderValue)> {
+        let mut client_builder = reqwest::Client::builder()
+            .danger_accept_invalid_certs(self.disable_https_validation);
+
+        if deploy_workarounds {
+            let bypass = self.bypass_info.as_ref().unwrap();
+            let proxy = reqwest::Proxy::all(bypass.proxy_url.clone())
+                .expect("Failed to create a proxy")
+                .basic_auth(&bypass.proxy_login.clone(), &bypass.proxy_password.clone());
+            client_builder =
+                client_builder.danger_accept_invalid_certs(true).proxy(proxy);
+        }
+
+        let client = client_builder.build().expect("Failed to build the HTTP client");
+
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert("user-agent", "curl/7.86.0".parse().unwrap());
+        headers.insert("accept", "*/*".parse().unwrap());
+
+        let image_response = client.get(image_url.clone()).headers(headers).send().await?;
+
+        let Some(mime_type) = image_response.headers().get("content-type").cloned() else {
+            bail!("Failed to get the content-type header for image: {image_url}");
+        };
+
+        // CloudFlare often returns CAPTCHAs for images, so we need to check for that.
+        let mime_str = String::from_utf8_lossy(mime_type.as_bytes());
+        if !mime_str.starts_with("image/") && !mime_str.starts_with("video/") {
+            return Err(CaptchaError { mime_type: mime_str.parse()? }.into());
+        }
+        Ok((image_response, mime_type))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use axum::{Extension, Router};
-use freshrss_image_cache_service_rs::app_service::AppService;
+use freshrss_image_cache_service_rs::app_service::{AppService, CloudFlareBypassProxy};
 use freshrss_image_cache_service_rs::handlers::root_router;
 use std::path::PathBuf;
 use std::sync::Arc;
@@ -15,6 +15,19 @@ async fn main() -> Result<()> {
     let images_dir = std::env::var("APP_IMAGES_DIR").with_context(|| "APP_IMAGES_DIR")?;
     let no_colors = std::env::var("APP_NO_ANSI_COLORS").is_err();
 
+    let cloudflare_proxy = std::env::var("APP_CLOUDFLARE_PROXY").ok();
+    let cloudflare_proxy_login = std::env::var("APP_CLOUDFLARE_PROXY_LOGIN").ok();
+    let cloudflare_proxy_pass = std::env::var("APP_CLOUDFLARE_PROXY_PASS").ok();
+
+    let bypass_info = match (cloudflare_proxy, cloudflare_proxy_login, cloudflare_proxy_pass) {
+        (Some(proxy), Some(login), Some(pass)) => Some(CloudFlareBypassProxy {
+            proxy_url: proxy,
+            proxy_login: login,
+            proxy_password: pass,
+        }),
+        _ => None,
+    };
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
     tracing_subscriber::fmt()
@@ -27,10 +40,23 @@ async fn main() -> Result<()> {
 
     info!("Listening on http://{}", listener.local_addr()?);
 
-    let app_service = Arc::new(AppService::new(access_token, PathBuf::from(images_dir)));
-    let app = Router::new()
+    let app_service = Arc::new(AppService::new(access_token.clone(),
+                                               PathBuf::from(images_dir.clone()),
+                                               false, bypass_info.clone()));
+    let safe = Router::new()
         .nest("/", root_router())
         .layer(Extension(app_service));
+
+    let unsafe_app = Arc::new(AppService::new(access_token.clone(),
+                                              PathBuf::from(images_dir.clone()),
+                                              true, bypass_info));
+    let unsafe_router = Router::new()
+        .nest("/", root_router())
+        .layer(Extension(unsafe_app));
+
+    let app = Router::new()
+        .nest("/notls", unsafe_router)
+        .nest("/", safe);
 
     axum::serve(listener, app).await?;
 
